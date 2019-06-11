f636c8de1e1f3bc9be91ef3dec8da952b0b0da50
Nisarg Patel <nisargp42@gmail.com>
2019-06-11 15:00:16 -0400

    Database seeder - Added

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -8,5 +8,10 @@ use Faker\Generator as Faker;
 $factory->define(Product::class, function (Faker $faker) {
     return [
         //
+        'name' => $faker->word,
+        'detail' => $faker->text($max = 100),
+        'price' => $faker->numberBetween(500,100),
+        'stock' => $faker->numberBetween(1,100),
+        'discount' => $faker->numberBetween(10,50),
     ];
 });
